e3fb0740585ee0a078bb36653b33d51e99bc1f1d
nelsonlobo99 <nelsonlobo@protonmail.com>
2019-09-07 19:21:53 +0530

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,177 @@
+#include<iostream>
+#include<time.h>
+#include<unistd.h>
+#include<stdio.h>
+//#include <ncurses.h>
+
+using namespace std;
+int tailLen=0;
+int tailX[100]={0},tailY[100]={0};
+bool gameOver;
+const int width=70, height = 20; 
+int score=0;
+int x,y;
+int fruitX, fruitY;
+enum eDir {STOP=0, LEFT, RIGHT, UP, DOWN};
+eDir dir;
+void Setup(){
+    gameOver = false;
+    dir = STOP;
+    x = width/2;
+    y = height/2;
+
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+    score = 0;
+}
+
+void printNelson(){
+    int matx[5][50] = {
+{0,0,0,0,0,1,0,0,0,1,0,1,1,1,1,0,1,0,0,0,0,1,1,1,1,0,1,1,1,1,0,0,0,0,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,},
+{0,0,0,0,1,1,0,0,1,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,1,0,0,0,0,1,1,0,0,1,0,0,0,0,0,0,0,0,0,0,0,},
+{0,0,0,1,0,1,0,1,0,0,0,1,1,1,1,0,1,0,0,0,0,1,1,1,1,0,1,0,0,1,0,0,0,1,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,},
+{0,0,1,0,0,1,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,1,0,1,0,0,1,0,0,1,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,},
+{0,1,0,0,0,1,0,0,0,0,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0,1,1,1,1,0,1,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,}
+    };
+
+    for (int i = 0; i < 5; i++)
+    {
+        for (int j = 0; j < 50; j++)
+        {
+            if(matx[i][j]==1){
+                cout<<"#";
+            }else
+            {
+                    cout<<" ";
+            }
+            
+        }
+        cout<<endl;
+        
+    }
+}
+void Draw(){
+    system("clear");
+    printNelson();
+    for(int i=0;i<width+1;i++)
+        cout << "#";
+    cout<<endl;
+
+    for(int i=0;i< height;i++){
+        for (int j = 0; j < width; j++){
+            if(j==0)
+                cout<<"#";
+            else {
+                if(i==fruitY && j==fruitX)
+                    cout<<"*";
+                else if(i==y && j==x)
+                    cout<<"X";
+                else{
+                    bool pri = false;
+                    for(int k=0;k<tailLen;k++){
+                        if(tailX[k] == j && tailY[k]==i){
+                            cout<<"*";
+                            pri = true;
+                        }
+                    }
+                    if(!pri)
+                        cout<<" ";
+                }
+                    
+            }
+                
+            
+            if(j==width-1)
+                cout<<"#";
+        }
+        cout<<endl;
+        
+    }
+
+    for(int i=0;i<width+1;i++)
+        cout << "#";
+    cout<<endl;
+
+    cout<<"SCORE:: "<<score<<endl;
+}
+void Input(){
+    char ch;
+
+    system("stty -echo"); // supress echo
+    system("stty cbreak"); // go to RAW mode
+    ch = getchar();
+    switch(ch){
+        case 'w':
+        case 'W':   if(dir!=DOWN)
+                        dir = UP;
+                    break;
+        case 's':   if(dir!=UP)
+        case 'S':       dir = DOWN;
+                    break;
+        case 'a':
+        case 'A':   if(dir!=RIGHT)
+                        dir = LEFT;
+                    break;
+        case 'd':
+        case 'D':   if(dir!=LEFT)
+                        dir = RIGHT;
+                    break;
+        case 'x':
+        case 'X':      
+                    gameOver = true;
+                    break;
+    }
+    system ("stty echo"); // Make echo work
+    system("stty -cbreak");// go to COOKED mode
+}
+void Logic(){
+    int prevX=tailX[0];
+    int prevY=tailY[0];
+    int prev2X,prev2Y;
+    tailX[0]=x;
+    tailY[0]=y;
+    for(int i=1;i<tailLen;i++){
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX; 
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+ switch(dir){
+    case LEFT:  x--;
+                break;
+    case RIGHT: x++;
+                break;
+    case UP:    y--;
+                break;
+    case DOWN:  y++;
+                break;
+    default: break;
+ }
+ if(x==fruitX && y == fruitY){
+    tailLen++;
+    score++;
+    fruitX = rand() % width;
+    fruitY = rand() % height;
+ }
+if(x==0 || y == -1 || x == width || y == height){
+    gameOver = true;
+ }
+
+ for(int k=1;k<tailLen;k++){
+     if(tailX[k]==x && tailY[k]==y)
+        gameOver = true;
+ }
+}
+int main(){
+    Setup();
+    while(!gameOver){
+        Draw();
+        Input();
+        Logic();
+        usleep(10000);
+    }
+    cout<<"GAMEOVER!!"<<endl;
+    return 0;
+}
